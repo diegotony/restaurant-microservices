a28458e46d1fb4776d8be4aaa9b4b3dc414be20b
diegotony <tucotony1396@gmail.com>
2019-12-06 00:46:39 -0500

    implentede with backend

--- staging/up-data.py
+++ staging/up-data.py
@@ -29,21 +29,21 @@ def request_data(service,body):
 if args.url:
     print('Adding to {0} ...'.format(args.url))
     # Added Tables
-    request_data('{0}/order/api-order/table'.format(args.url),{'name':"Mesa1"})
-    request_data('{0}/order/api-order/table'.format(args.url),{'name':"Mesa2"})
-    request_data('{0}/order/api-order/table'.format(args.url),{'name':"Mesa3"})
+    request_data('{0}/order/table'.format(args.url),{'name':"Mesa1"})
+    request_data('{0}/order/table'.format(args.url),{'name':"Mesa2"})
+    request_data('{0}/order/table'.format(args.url),{'name':"Mesa3"})
 
     # Added Catalog
-    almuerzos = request_data_json('{0}/order/api-order/catalog'.format(args.url),{'name':"Almuerzos"})
-    desayunos = request_data_json('{0}/order/api-order/catalog'.format(args.url),{'name':"Desayunos"})
+    almuerzos = request_data_json('{0}/order/catalog'.format(args.url),{'name':"Almuerzos"})
+    desayunos = request_data_json('{0}/order/catalog'.format(args.url),{'name':"Desayunos"})
 
     # Added Items
 
-    item1 = request_data_json('{0}/order/api-order/item'.format(args.url),{'name':"Combo 1","description":"Este combo incluye ...","price":2.5,"catalog":desayunos['_id']})
-    item2 = request_data_json('{0}/order/api-order/item'.format(args.url),{'name':"Combo 2","description":"Este combo incluye ...","price":3.5,"catalog":desayunos['_id']})
+    item1 = request_data_json('{0}/order/item'.format(args.url),{'name':"Combo 1","description":"Este combo incluye ...","price":2.5,"catalog":desayunos['_id']})
+    item2 = request_data_json('{0}/order/item'.format(args.url),{'name':"Combo 2","description":"Este combo incluye ...","price":3.5,"catalog":desayunos['_id']})
 
     # Added Users
 
-    user = request_data_json('{0}/client/api-client/users'.format(args.url),{"first_name": "test","last_name": "test","email": "tucotony1396gmail.com","dni": "1150601100","password": "nope"})
+    user = request_data_json('{0}/client'.format(args.url),{"first_name": "test","last_name": "test","email": "tucotony1396gmail.com","dni": "1150601100","password": "nope"})
    
     print(":3")
