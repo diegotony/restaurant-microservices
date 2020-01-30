1110373d71e1c503813509640f0c360094b9d5e0
diegotony <tucotony1396@gmail.com>
2020-01-30 03:31:18 -0500

    all ok

--- k8s/Security/scripts/create_kong.sh
+++ k8s/Security/scripts/create_kong.sh
@@ -1,30 +1,31 @@
+kubectl apply -f kong/kong.yml
 kubectl apply -f kong/paths.yml
 kubectl apply -f kong/plugins.yml
 
 kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "rl-by-ip\n"}}}'
 kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-id\n"}}}'
 kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "cors\n"}}}'
-kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
-kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
+# kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
+# kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
 
 kubectl patch svc client-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "rl-by-ip\n"}}}'
 kubectl patch svc client-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-id\n"}}}'
 kubectl patch svc client-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "cors\n"}}}'
-kubectl patch svc client-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
-kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
+# kubectl patch svc client-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
+# kubectl patch svc client-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
 
 kubectl patch svc fee-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "rl-by-ip\n"}}}'
 kubectl patch svc fee-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-id\n"}}}'
 kubectl patch svc fee-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "cors\n"}}}'
-kubectl patch svc fee-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
-kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
+# kubectl patch svc fee-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
+# kubectl patch svc fee-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
 
 
 kubectl patch svc orden-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "rl-by-ip\n"}}}'
 kubectl patch svc orden-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-id\n"}}}'
 kubectl patch svc orden-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "cors\n"}}}'
-kubectl patch svc orden-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
-kubectl patch svc billing-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
+# kubectl patch svc orden-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "request-termination\n"}}}'
+# kubectl patch svc orden-api -p '{"metadata":{"annotations":{"plugins.konghq.com": "bot-detection\n"}}}'
 
 
 #kubectl create configmap kongconfig --from-file=gateway/conf.d
